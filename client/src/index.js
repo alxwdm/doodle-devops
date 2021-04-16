cc78dcf13c74e5019da0c6b4611127e90727d92f
alxwdm <58976179+alxwdm@users.noreply.github.com>
2021-04-16 12:56:51 +0200

    feat: Use tf image rescaling
    feat: Use tf image rescaling
    
    Use tf.image.resizeBilinear instead of recaledCanvas to transform canvas drawing into correctly sized image for prediction.

--- client/src/index.js
+++ client/src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from "react-dom";
 import CanvasDraw from "react-canvas-draw";
 import classNames from "./index.css";
 import * as tf from "@tensorflow/tfjs";
-//import * as tfn from "@tensorflow/tfjs-node"
 
-//import { useIsMobileOrTablet } from "./utils/isMobileOrTablet";
 // import "./styles.css";
 
 class App extends Component {
-//  const isMobOrTab = useIsMobileOrTablet();
   state = {
     color: "#000000",
     width: 400,
     height: 400,
-    brushRadius: 10,
+    brushRadius: 8,
     lazyRadius: 0,
     model: null,
     metadata: null,
@@ -24,9 +21,9 @@ class App extends Component {
   };
 
   categories = [
-    'Cat',
-    'Dog',
-    'Mouse'
+    'cat',
+    'dog',
+    'mouse'
   ];
 
   img_test = null;
@@ -34,6 +31,8 @@ class App extends Component {
   async loadModel() {
     try {
       /*
+      Loads a TensorFlow json model from a server.
+
       Example Code:
       - server: https://codesandbox.io/s/upbeat-lumiere-qyeho?file=/src/index.js
       - client: https://codesandbox.io/s/brave-murdock-ck6of?file=/src/App.js
@@ -54,13 +53,17 @@ class App extends Component {
     }}
 
   random_choice() {
+    /* 
+    Selects a random category
+    */
     const new_idx = Math.floor(Math.random()*this.categories.length);
     this.setState({category_idx: new_idx});
-    //console.log('called random_choice. State is: ', this.state.category_idx);
   }
 
   dummy_predict() {
-    // dummy prediction
+    /*
+    Makes a dummy prediction on empty tensor
+    */
     const xs = tf.zeros([1, 28, 28, 1]);
     const result = this.state.model.predict(xs).squeeze();
     const tf_idx = result.argMax(0);
@@ -72,57 +75,48 @@ class App extends Component {
   }
 
   async canvas_to_tensor(imgloaded_callback) {
+    /*
+    Gets canvas data and converts it into a tensor (via an Image).
+    Also rescales the image data to (28,28) and reduces the dimensions.
+    */
     var img = new Image();
-    img.src = this.rescaledCanvas.canvasContainer.children[1].toDataURL();
-    img.width = 28;
-    img.height = 28;
+    img.src = this.loadableCanvas.canvasContainer.children[1].toDataURL();
+    img.width = 400;
+    img.height = 400;
     const model = this.state.model;
     img.onload = () => {
       var tf_img = tf.browser.fromPixels(img, 4);
+      tf_img = tf.image.resizeBilinear(tf_img, [28, 28]);
       tf_img = tf_img.slice([0, 0, 3], [-1, -1, -1]);
-      const pred_idx_res = imgloaded_callback(model, tf_img);
+      imgloaded_callback(model, tf_img);
     }
   }
 
   async model_predict() {
-          console.log('Predicting...');
-          const pred_idx_ret = this.canvas_to_tensor(
-            (model, tf_img) => {
-              //console.log(tf_img.print());
-              console.log('i am inside callback fn');
-              // predict on image tensor
-              tf_img = tf.expandDims(tf_img, 0);
-              const pred_logits = model.predict(tf_img).squeeze();
-              const pred_idx_tf = pred_logits.argMax(0);
-              // output prediction to console -- tensors
-              //console.log(pred_logits.print(true));
-              //console.log(pred_idx_tf.print(true));   
-              // output prediction to console - array
-              const pred_idx = pred_idx_tf.dataSync();
-              console.log('Prediction is: ' + pred_idx[0]);
-              // TODO set state inside this fn without error
-              this.setState({predict_idx: pred_idx});
-              this.setState({predict_valid: true});
-              return pred_idx;
-            }
-            );
+    /*
+    Predicts on canvas data.
+    */
+    console.log('Predicting...');
+    this.canvas_to_tensor(
+      (model, tf_img) => {
+        // predict on image tensor
+        tf_img = tf.expandDims(tf_img, 0);
+        const pred_logits = model.predict(tf_img).squeeze();
+        const pred_idx_tf = pred_logits.argMax(0);
+        // output prediction to console -- tensors
+        //console.log(pred_logits.print(true));
+        //console.log(pred_idx_tf.print(true));   
+        // output prediction to console - array
+        const pred_idx = pred_idx_tf.dataSync();
+        this.setState({predict_idx: pred_idx});
+        this.setState({predict_valid: true});
+        // log prediction output to console
+        console.log('Prediction is: ' + pred_idx[0]);
+        console.log(this.categories[this.state.predict_idx]);
+        return pred_idx;
+      }
+      );
   }
-
-/*
-  model_predict(tf_img) {
-    // replace this with canvas_to_tensor
-    //var tf_img = tf.zeros([28, 28, 1]);
-    // predict on image tensor
-    tf_img = tf.expandDims(tf_img, 0);
-    const pred_logits = this.state.model.predict(tf_img).squeeze();
-    const pred_idx = pred_logits.argMax(0);
-    // output prediction to console
-    console.log(pred_logits.print(true));
-    console.log(pred_idx.print());   
-    this.setState({predict_idx: pred_idx.dataSync()});
-    return pred_idx.dataSync();
-  }
-*/
 
 /*
   draw_image() {
@@ -137,7 +131,7 @@ class App extends Component {
   render(){
   return (
     <div className="App">
-      <h1>Doodle DevOps Client</h1>
+      <h1>Cat, dog or mouse - can an AI recognize your drawing?</h1>
 
       <p>
         Can you draw a {this.categories[this.state.category_idx]} for me?
@@ -200,14 +194,10 @@ class App extends Component {
             );
         await this.loadableCanvas.loadSaveData(
             localStorage.getItem("savedDrawing")
-          );
-        await this.rescaledCanvas.loadSaveData(
-            localStorage.getItem("savedDrawing")
-          );  
-        await this.model_predict();      
+          );       
         }}
       >
-        Predict
+        Done drawing.
       </button>
 
       <p>
@@ -223,7 +213,7 @@ class App extends Component {
           this.model_predict();      
         }}
       >
-        Predict again!
+        Make a prediction!
       </button>
 
       <CanvasDraw
@@ -237,25 +227,9 @@ class App extends Component {
         disabled={true}
         hideGrid={true}
       />
-
-      <CanvasDraw 
-        ref={canvasDraw => (this.rescaledCanvas = canvasDraw)}
-        brushColor={this.state.color}
-        brushRadius={1}
-        lazyRadius={0}
-        canvasWidth={28}
-        canvasHeight={28}
-        saveData={localStorage.getItem("savedDrawing")}
-        disabled={true}
-        //style={{"display": "none"}}
-      />
-
       <p>
         This is a {this.categories[this.state.predict_idx]}!
       </p>
-
-
-
 
       <div id="icon" style={{"color": "grey", "fontSize": 8+'px'}}>>
       Icon made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> {' '}
@@ -270,35 +244,3 @@ export default App;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
-        /*
-Example Tools to set cursor width etc.
-          <label>Width:</label>
-          <input
-            type="number"
-            value={this.state.width}
-            onChange={e =>
-              this.setState({ width: parseInt(e.target.value, 10) })
-            }
-          />
-        </div>
-        <div>
-          <label>Height:</label>
-          <input
-            type="number"
-            value={this.state.height}
-            onChange={e =>
-              this.setState({ height: parseInt(e.target.value, 10) })
-            }
-          />
-        </div>
-        <div>
-          <label>Brush-Radius:</label>
-          <input
-            type="number"
-            value={this.state.brushRadius}
-            onChange={e =>
-              this.setState({ brushRadius: parseInt(e.target.value, 10) })
-            }
-          />
-        */
