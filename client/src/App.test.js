e4dc7197dfa88a0342c77f96bbfbb6a6e2d1640b
alxwdm <58976179+alxwdm@users.noreply.github.com>
2021-04-16 17:51:19 +0200

    test: Add App.test.js

---
+++ client/src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('Dummy Test', () => {});
+
+/*
+test('renders without fault', () => {
+	render(<App />);
+  	//const linkElement = screen.getByText(/learn react/i);
+  	//expect(linkElement).toBeInTheDocument();
+});
+*/
