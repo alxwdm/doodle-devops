4f364362422675c399954dec369e10b6b7be016a
alxwdm <58976179+alxwdm@users.noreply.github.com>
2021-04-22 12:44:44 +0200

    feat: read_dataset fn

--- trnsvr/trainer/model.py
+++ trnsvr/trainer/model.py
@@ -1,22 +1,33 @@
 # model.py for delta-training
-#import tensorflow as tf
+import tensorflow as tf
 import psycopg2
 import numpy as np
+from keys import keys
 
 MODEL_DIR = './model/'
+BATCH_SIZE = 128
+BUFFER_SIZE = 256
+TRAIN_STEPS = 2
+KEYS = keys
 
-def get_data_from_db(keys, mode='train', batch_size=-1):
+def get_data_from_db(mode='train', limit=10000, batch_size=-1):
     """
     Connect to the PostgreSQL database server and
     retrieve data from train or test split according to mode.
+    The most recent data is queried using "limit".
     Data is retrieved in batches of batch_size.
-    To load everything in one step, set batch_size to -1.
+    To load everything in one step, set batch_size to -1 (default).
     """
+    # convert string types to fit tf args input (and to avoid SQL injection)
+    if isinstance(mode, bytes):
+        mode = mode.decode('utf-8')
+    else:
+        mode = str(mode)
     conn = None
     try:
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**keys)
+        conn = psycopg2.connect(**KEYS)
         # test connection by printing db version 
         with conn.cursor() as curs:
             curs.execute('SELECT version()')
@@ -27,26 +38,37 @@ def get_data_from_db(keys, mode='train', batch_size=-1):
         with conn.cursor() as curs:
             curs.execute(
                 '''
-                SELECT idx, pred, data 
+                SELECT idx, data 
                 FROM doodles 
                 WHERE split = \'{}\'
-                '''.format(mode))
+                ORDER BY insert_date DESC
+                LIMIT {}
+                '''.format(mode, limit))
             # fetch data depending on batch_size setting
-            # TODO: rewrite this
-            while True:
-                # fetch all and return results
-                if batch_size<0:
-                    results = curs.fetchall()
-                    idces, preds, data = map(list, zip(*results))
-                    return (idces, preds, data)
-                # fetch batch and yield results
-                else:
+            if batch_size<0:
+                results = curs.fetchall()
+                idces, data = map(list, zip(*results))
+                # yield one feature, label pair at a time
+                for idx, img in zip(idces, data):
+                    yield img, [idx]
+            else:
+                # fetch first batch
+                results = curs.fetchmany(batch_size)
+                idces, data = map(list, zip(*results))
+                # yield one feature, label pair at a time
+                for idx, img in zip(idces, data):
+                    yield img, [idx]
+                # fetch consecutive batches
+                while results:
                     results = curs.fetchmany(batch_size)
-                    if not results:
-                        break
-                    idces, preds, data = map(list, zip(*results))
-                    # TODO turn into generator
-                    raise NotImplementedError
+                    idces, data = map(list, zip(*results))
+                    # yield one feature, label pair at a time
+                    for idx, img in zip(idces, data):
+                        yield img, [idx]
+    # error empty database
+    except ValueError:
+        print('Database appears to be empty.')
+        return None                      
     # error during connection
     except (Exception, psycopg2.DatabaseError) as error:
         print('Database connection failed.')
@@ -57,12 +79,34 @@ def get_data_from_db(keys, mode='train', batch_size=-1):
             conn.close()
             print('Database connection closed.')
 
-def read_dataset(mode, train_steps):
-    def _input_fn():
-        # TODO
-        dataset = None
-        return dataset
-    return _input_fn
+def read_dataset(mode='train'):
+    """
+    Gets data from PostgreSQL DB and converts it into a tf.data.Dataset.
+    """
+    # preprocessing functions
+    def _fixup_shape(features, label):
+        features = tf.reshape(features, [28, 28, 1])
+        return features, label
+    def _normalize(features, label):
+        features = tf.math.divide(features, 255)
+        return features, label
+
+    # create tf.Dataset by querying database
+    ds = tf.data.Dataset.from_generator(
+            get_data_from_db,
+            args=[mode], 
+            output_types=(tf.float32, tf.int32)
+        )
+    # apply preprocessing steps
+    ds = ds.map(_fixup_shape) \
+           .map(_normalize)
+    # shuffle, batch and repeat depending on mode
+    if mode == 'train':
+        ds = ds.shuffle(buffer_size=BUFFER_SIZE).batch(BATCH_SIZE).prefetch(2)
+    else:
+        ds = ds.batch(BATCH_SIZE).prefetch(2)
+
+    return ds
 
 def train_and_evaluate():
     # TODO
--- trnsvr/trainer/task.py
+++ trnsvr/trainer/task.py
@@ -1,13 +1,19 @@
 # task.py for delta-training
 import numpy as np
-from keys import keys
-from model import get_data_from_db
+import tensorflow as tf
+from model import get_data_from_db, read_dataset
 
 if __name__ == '__main__':
+
     # test retrieving data from db
-    idces, preds, data = get_data_from_db(keys)
-    print(type(data))
-    idces = np.array(idces)
-    preds = np.array(preds)
-    data = np.array(data)
-    print('shapes:', idces.shape, preds.shape, data.shape)
+    data_gen = get_data_from_db()
+    for data, idces in data_gen:
+        idces = np.array(idces)
+        data = np.array(data)
+        print('shapes:', idces.shape, data.shape)
+
+    # test tensorflow data pipeline
+    ds = read_dataset(mode='train')
+    for feat, label in ds.take(1):
+        print(tf.shape(feat), tf.shape(label))
+        #print(feat, label)
