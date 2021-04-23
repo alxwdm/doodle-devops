0e8885c3357675cfb2ad47e3b32eb9bf196d174c
alxwdm <58976179+alxwdm@users.noreply.github.com>
2021-04-23 18:57:03 +0200

    feat: Add com setup and training fn

--- trnsvr/app.py
+++ trnsvr/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+
+PORT = 5000
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/test')
 def hello():
-	return "Hi, trnsvr here!"
+    return 'Hi, trnsvr here!'
+
+@app.route('/train', methods=['GET', 'POST'])
+def perform_training():
+    if request.method == 'POST':
+        # TOOD
+        return 'training started'
+    return 'training GET request'
 
 if __name__ == '__main__':
-	app.run()
+    app.config['SERVER_NAME'] = 'trnsvr:5000'
+    app.run(host='0.0.0.0', port=PORT)
